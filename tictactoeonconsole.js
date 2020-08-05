b3638ea5a01c3db34d862dea229bf0f6d7883bde
GK <54536253+lollipas@users.noreply.github.com>
2020-08-05 14:56:22 +0300

    Fixed bugs, improved stability

--- tictactoeonconsole.js
+++ tictactoeonconsole.js
@@ -1,4 +1,5 @@
 // Playing field
+"use strict"
 var grid = [
     
     ['','','',],
@@ -9,8 +10,8 @@ let coords;
 
 console.table(grid)
 
-playerInput = document.querySelector('#input1')
-playerSumbit= document.querySelector('#button1')
+let playerInput = document.querySelector('#input1')
+let playerSumbit = document.querySelector('#button1')
 
 button1.addEventListener('click', playerMove)
 
@@ -22,37 +23,50 @@ function playerMove(){
     //Parsing the 2D array
     for(let i = 0; i < grid.length; i++){
         for(let j = 0; i < grid[i].length; j++){
-            
-            if(coords[0] === 'A'){         
-               grid[y][j] = 'X'       
+            if(grid[i][j] != "X" && grid[i][j] != "O"){
+                if(coords[0] === 'A'){ 
+                    grid[y][j] = 'X'         
+                 }
+                 
+                 if(coords[0] === 'B'){
+                     grid[y][j+1] = 'X'  
+                 }
+     
+                 if(coords[0] === 'C'){
+                     grid[y][j+2] = 'X'
+                 }     
             }
-            
-            if(coords[0] === 'B'){
-                grid[y][j+1] = 'X'  
-            }
-
-            if(coords[0] === 'C'){
-                grid[y][j+2] = 'X'
-            }
-               break
-            }
+        break       
         }
+    }
+    
     opponentMove()
     updateGrid()
     checkForPlayerVictory()
     checkForOpponentVictory()
+}
 
-    if (checkForOpponentVictory == true){
-        console.log("CPU wins!")
-    }
-
-    if (checkForPlayerVictory == true){
-        
-        console.log("Player wins")
-    }
-
-    }
+function rng() {
+    return Math.floor(Math.random() * 3);
+}
+function opponentMove(){
+    let randomNumberX = rng()
+    let randomNumberY = rng()
+    console.log(randomNumberY)
     
+    if(grid[randomNumberX][randomNumberY] === "O" || grid[randomNumberX][randomNumberY] === "X"){
+        let randomNumberX = rng()
+        let randomNumberY = rng()
+        grid[randomNumberX][randomNumberY] = "O"
+    } else if (grid[randomNumberX][randomNumberY] === "O" && grid[randomNumberX][randomNumberY] === "X") {
+        let randomNumberX = rng()
+        let randomNumberY = rng()
+        grid[randomNumberX][randomNumberY] = "O"
+    } else {
+        grid[randomNumberX][randomNumberY] = "O"
+    }
+}
+
 function updateGrid(){
     console.table(grid)
 }
@@ -61,23 +75,22 @@ function checkForPlayerVictory(){
     for(let i = 0; i < 3; i++){
         for(let j = 0; i < 3; j++){
             if(grid[i][j] === "X" && grid[i+1][j] === "X" && grid[i+2][j] === "X"){ //Columns
-                //console.log("You win!")
-                return true;
+                console.log("You win!")
             }
 
             if(grid[i][j] === "X" && grid[i][j+1] === "X" && grid[i][j+2] === "X"){ //Rows
-                //console.log("You win!")
-                return true;
+                console.log("You win!")
+                
             }
 
             if(grid[0][0] === "X" && grid[1][1] === "X" && grid[2][2] === "X"){ //Diagonal 1
-                //console.log("You win!")
-                return true;
+                console.log("You win!")
+               
             }
 
             if(grid[0][2] === "X" && grid[1][1] === "X" && grid[0][2] === "X"){ //Diagonal 2
-                //console.log("You win!")
-                return true;
+                console.log("You win!")
+                
             }
             break
         }
@@ -89,35 +102,20 @@ function checkForOpponentVictory(){
         for(let j = 0; i < 3; j++){
             if(grid[i][j] === "O" && grid[i+1][j] === "O" && grid[i+2][j] === "O"){ //Columns
                 console.log("CPU wins!")
-                return true;
             }
 
             if(grid[i][j] === "O" && grid[i][j+1] === "O" && grid[i][j+2] === "O"){ //Rows
                 console.log("CPU wins!")
-                return true;
             }
 
             if(grid[0][0] === "O" && grid[1][1] === "O" && grid[2][2] === "O"){ //Diagonal 1
                 console.log("CPU wins!")
-                return true;
             }
 
             if(grid[0][2] === "O" && grid[1][1] === "O" && grid[0][2] === "O"){ //Diagonal 2
                 console.log("CPU wins!")
-                return true;
             }
             break
         }
-    }
-}
-
-function opponentMove(){
-    var randomNumber = Math.floor(Math.random() * 3);
-    
-    if(grid[randomNumber][randomNumber] === "X"){
-        opponentMove()
-    }
-    else{
-        grid[randomNumber][randomNumber] = "O"
     }
 }
